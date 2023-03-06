b0454900ea64a3ddc432bd176cfaf5f9137bd895
Augusto Lopez <augusto.lopez@estudante.senairs.edu.br>
2023-03-05 23:51:09 -0300

    Subindo projeto remodelado...

--- js/login.js
+++ js/login.js
@@ -1,20 +1,21 @@
-const input = document.querySelector('.login_input');
-const button = document.querySelector('.login_button');
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
 
-
 const validateInput = ({ target }) => {
-    if (target.value.length > 2) {
+    if (target.value.length > 3) {
         button.removeAttribute('disabled');
         return;
     }
+
     button.setAttribute('disabled', '');
-};
+}
 
 const handleSubmit = (event) => {
     event.preventDefault();
 
     localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html';
 }
 
 input.addEventListener('input', validateInput);
